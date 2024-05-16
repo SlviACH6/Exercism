734102efd6550b2d7948dee9caa1cec490fc44c8
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-05-16 17:51:49 +0200

    new exercises

--- starting.js
+++ starting.js
@@ -73,9 +73,63 @@ Implement the calculateResellPrice(originalPrice, age) function that applies thi
 }*/
 
 export function calculateResellPrice(originalPrice, age) {
-return age < 3 ? originalPrice * 0.8 : age === 3 || age < 10 ? originalPrice * 0.7 : originalPrice * 0.5
+  return age < 3
+    ? originalPrice * 0.8
+    : age === 3 || age < 10
+    ? originalPrice * 0.7
+    : originalPrice * 0.5;
 }
 
 console.log(calculateResellPrice(1000, 1));
 console.log(calculateResellPrice(1000, 5));
 console.log(calculateResellPrice(1000, 18));
+
+/*DTOTAL NUMBER OF BIRDS
+Let us start analyzing the data by getting a high-level view. Find out how many birds 
+you counted in total since you started your logs.
+Implement a function totalBirdCount that accepts an array that contains 
+the bird count per day. It should return the total number of birds that you counted.*/
+
+export function totalBirdCount(birdsPerDay) {
+  let count = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    count += birdsPerDay[i];
+  }
+  return count;
+}
+
+console.log(totalBirdCount([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1]));
+console.log(totalBirdCount([2, 5, 0, 7, 4, 1, 3, 1, 3, 1]));
+
+/*BIRDS PER WEEK
+Now that you got a general feel for your bird count numbers, you want to make a more fine-grained analysis.
+Implement a function birdsInWeek that accepts an array of bird counts per day and a week number. 
+It returns the total number of birds that you counted in that specific week. You can assume weeks are always tracked completely.*/
+
+export function birdsInWeek(birdsPerDay, week) {
+  const daysPerWeek = 7;
+  let weekStarts = (week - 1) * daysPerWeek;
+  let weekEnds = weekStarts + daysPerWeek - 1;
+  let count = 0;
+
+  for (let i = weekStarts; i <= weekEnds; i++) {
+    count += birdsPerDay[i];
+  }
+  return count;
+}
+
+console.log(birdsInWeek([3, 0, 5, 1, 0, 4, 1, 0, 3, 4, 3, 0, 8, 0], 1));
+
+/*FIX COUNTING MISTAKE
+You realized that all the time you were trying to keep track of the birds, there was one hiding in a far corner of the garden. You figured out that this bird always spent every second day in your garden. You do not know exactly where it was in between those days but definitely not in your garden. Your bird watcher intuition also tells you that the bird was in your garden on the first day that you tracked in your list.
+
+Given this new information, write a function fixBirdCountLog that takes an array of birds counted per day as an argument. It should correct the counting mistake and return the modified array.*/
+
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length; i+=2) {
+     birdsPerDay[i]++
+  }
+  return birdsPerDay;
+}
+ console.log(fixBirdCountLog([2, 5, 0, 7, 4, 1]))
+ console.log(fixBirdCountLog([4, 2]))
